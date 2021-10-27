5d18a5144e339044f7164dd2fd106fff964c3d06
aventusm <8antonm@gmail.com>
2021-10-27 17:25:19 +0300

    13.17

---
+++ migrations/20212710_00_initialize_notes_and_users.js
@@ -0,0 +1,83 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable('notes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      important: {
+        type: DataTypes.BOOLEAN,
+      },
+      date: {
+        type: DataTypes.DATE,
+      },
+    });
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+      },
+    });
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+    });
+    await queryInterface.addColumn('notes', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    });
+    await queryInterface.addColumn('blogs', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('notes');
+    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('blogs');
+  },
+};
--- models/index.js
+++ models/index.js
@@ -2,18 +2,10 @@ const Note = require('./note');
 const Blog = require('./blog');
 const User = require('./user');
 
-Note.sync();
-Blog.sync();
-User.sync();
-
 User.hasMany(Note);
 User.hasMany(Blog);
 Note.belongsTo(User);
 Blog.belongsTo(User);
-
-User.sync({ alter: true });
-Note.sync({ alter: true });
-Blog.sync({ alter: true });
 
 module.exports = {
   Note,
